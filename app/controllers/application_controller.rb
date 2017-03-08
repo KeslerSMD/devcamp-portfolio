212a8c7a2bf7ebf20a8008a55fbfb554843b92cb
Devan Walker <devanrwalker@gmail.com>
2017-03-08 13:23:39 -0700

    saved my view gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,4 @@ class ApplicationController < ActionController::Base
   include SetSource
   include CurrentUser
   include DefaultPageContent
-
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,4 +16,8 @@ module ApplicationHelper
       content_tag(:p, greeting, class: "source-greeting")
     end
   end
+
+  def copyright_generator
+    @copyright = WalkerViewTool::Renderer.copyright 'Devan Walker', 'All rights reserved'
+  end
 end
