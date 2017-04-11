de581c728dad9a0fe277784dcabd0f9606f80c3f
Devan Walker <devanrwalker@gmail.com>
2017-04-10 20:57:27 -0600

    added relic

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require jquery_ujs
 //= require jquery-ui
 //= require html.sortable
+//= require contact
 //= require cocoon
 //= require gritter
 //= require cable
---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,12 @@
+class ContactsController < ApplicationController
+
+	layout "contact"
+
+	def index
+
+	end
+
+	def show
+
+	end
+end
